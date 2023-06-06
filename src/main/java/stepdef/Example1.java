74a85f0d827198cf3cfadb34aaee9b7754bc5496
rmnelapatla70 <rama_70@yahoo.com>
2023-06-06 08:18:51 +0530

    change by ramana

--- src/main/java/stepdef/Example1.java
+++ src/main/java/stepdef/Example1.java
@@ -2,10 +2,11 @@ package stepdef;
 
 public class Example1 {
     public static void main(String[] args) {
-        System.out.println("print");
+        System.out.println("new test");
         int i=0;
         do{
             System.out.println(i);
         }while(i<10);
+        System.out.println("test123");
     }
 }
