a39d4b69e3273fcf27cae39e2d27b398fbc25463
trushna8585 <trushna8585@gmail.com>
2023-06-16 21:10:44 -0500

    add do while loop

--- src/main/java/stepdef/traininggit.java
+++ src/main/java/stepdef/traininggit.java
@@ -7,5 +7,9 @@ public class traininggit {
     public static void main(String ar[]) {
 
         System.out.println("welcome to java");
+        int i=0;
+        do{
+            i++;
+        }while (i<10);
     }
 }
